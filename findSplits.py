8e2017c6a508dd5a00e03823f4fdf4ffb1f4086b
Ian Weber <iacweber@gmail.com>
2025-07-18 22:45:54 -0700

    first draft of set release filter

--- findSplits.py
+++ findSplits.py
@@ -2,6 +2,26 @@ import csv
 
 # === Load Data ===
 cards = []
+set_release_order = {
+    "lea": 0, "leb": 1, "2ed": 2, "arn": 3, "atq": 4, "3ed": 5, "leg": 6, "drk": 7,
+    "fem": 8, "4ed": 9, "ice": 10, "chr": 11, "hom": 12, "all": 13, "mir": 14,
+    "vis": 15, "wth": 16, "tmp": 17, "sth": 18, "exo": 19, "usg": 20, "ulg": 21,
+    "uds": 22, "mmq": 23, "nem": 24, "pcy": 25, "inv": 26, "pls": 27, "apc": 28,
+    "ody": 29, "tor": 30, "jud": 31, "ons": 32, "lgd": 33, "scg": 34, "mrd": 35,
+    "dst": 36, "5dn": 37, "chk": 38, "bok": 39, "sok": 40, "rav": 41, "gpt": 42,
+    "dis": 43, "csp": 44, "tsp": 45, "plc": 46, "fut": 47, "lrw": 48, "mor": 49,
+    "shm": 50, "eve": 51, "ala": 52, "con": 53, "arb": 54, "m10": 55, "zen": 56,
+    "wwk": 57, "roe": 58, "m11": 59, "som": 60, "mbs": 61, "nph": 62, "m12": 63,
+    "isd": 64, "dka": 65, "avr": 66, "m13": 67, "rtr": 68, "gtc": 69, "dgm": 70,
+    "m14": 71, "ths": 72, "bng": 73, "jou": 74, "m15": 75, "ktk": 76, "frf": 77,
+    "dtk": 78, "ori": 79, "bfz": 80, "ogw": 81, "soi": 82, "emn": 83, "kld": 84,
+    "aer": 85, "akh": 86, "hou": 87, "xln": 88, "rix": 89, "dom": 90, "grn": 91,
+    "rna": 92, "war": 93, "m20": 94, "eld": 95, "thb": 96, "iko": 97, "znr": 98,
+    "khm": 99, "stx": 100, "afr": 101, "mid": 102, "vow": 103, "neo": 104,
+    "snc": 105, "dmu": 106, "bro": 107, "one": 108, "mom": 109, "woe": 110,
+    "lci": 111, "otj": 112, "mkm": 113,
+}
+
 with open("first_printings_trimmed_scraped_types.csv", newline='', encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
