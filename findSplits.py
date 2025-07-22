09b829e35d2f6fbc74f816b519ee2145d0162ea2
Ian Weber <iacweber@gmail.com>
2025-07-21 23:49:58 -0700

    I should commit before midnight

--- findSplits.py
+++ findSplits.py
@@ -35,6 +35,7 @@ set_release_order = {'LEA': 1, 'LEB': 2, 'ARN': 3, 'ATQ': 4, 'LEG': 5, 'DRK': 6,
 # structuring plan: sets are grouped by year, and sorted within year by release date
 # Cards are sorted by color within sets, and within color groupings sorted by mana value
 # this allows splits on color, on year, and on mv, with compute time on type being more challenging
+# I'll implement this thursday.
 
 with open("first_printings_trimmed_scraped_types.csv", newline='', encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
