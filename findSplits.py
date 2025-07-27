3e103f9f9eb9b95315c817c0f9b6401c76e5dcad
Ian Weber <iacweber@gmail.com>
2025-07-26 23:34:04 -0700

    we do be planning

--- findSplits.py
+++ findSplits.py
@@ -53,6 +53,7 @@ set_release_order = {'LEA': 1, 'LEB': 2, 'ARN': 3, 'ATQ': 4, 'LEG': 5, 'DRK': 6,
 # this can be done with the first card from the dataset for each set being put into the year bucket
 # each card follows in line
 # each year index of the outer array holds an array of set objects
+# each set object holds an array of colorlist objects
 
 with open("first_printings_trimmed_scraped_types.csv", newline='', encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
